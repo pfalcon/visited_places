24f4a1e67514dfbfd432d785ea457746083d90b2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-09-25 20:45:18 +0300

    countries.js, cities.js: Add more sample entries.
    Based on James Bond movies locations,
    https://en.wikipedia.org/wiki/List_of_James_Bond_film_locations

--- countries.js
+++ countries.js
@@ -1,5 +1,7 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
+    ITA: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
+    THA: {fillKey: 'visited'},
 }
